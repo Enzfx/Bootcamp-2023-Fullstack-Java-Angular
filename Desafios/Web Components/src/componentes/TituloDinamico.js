a7451366c3cff78a43fa4ffe160397278e184b8d
Enzo Freitas <genzofreitas@gmail.com>
2025-06-08 14:40:45 -0300

    Update

---
+++ Desafios/Web Components/src/componentes/TituloDinamico.js
@@ -0,0 +1,9 @@
+class TituloDinamico extends HTMLElement {
+  constructor() {
+    super();
+    const shadow = this.attachShadow({ mode: 'open' });
+    // Criar a base do componente
+    // Estilizar o componente
+    // Enviar para o shadow DOM
+    }
+}
