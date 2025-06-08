f8284c76c8653c9af7797a2a97dcca79f71a8d32
Enzo Freitas <genzofreitas@gmail.com>
2025-06-08 18:44:22 -0300

    Update

--- Desafios/Web Components/src/componentes/CardNesw.js
+++ Desafios/Web Components/src/componentes/CardNesw.js
@@ -1,8 +1,41 @@
 class CardNews extends HTMLElement {
     constructor() {
         super();
-        const shadow = this.attachShadow({ mode: 'open' });
-        shadow.innerHTML = "<h1>Teste</h1>";
-    }}
-customElements.define('card-news', CardNews);
+        const shadow = this.attachShadow({ mode: 'open' });    
+        shadow.appendChild(this.build());
+        shadow.appendChild(this.style());
+    }
 
+    build () {
+        const componenteRoot = document.createElement('div');
+        componenteRoot.setAttribute('class', 'card');
+
+        const left = document.createElement('div');
+        left.setAttribute('class', 'left');
+
+        const autor = document.createElement('span');
+        const titulo = document.createElement('h1');
+        const conteudo = document.createElement('p');
+
+        left.appendChild(autor)
+        left.appendChild(titulo);
+        left.appendChild(conteudo);
+
+        const right = document.createElement('div');
+        right.setAttribute('class', 'right');
+        const Imagem = document.createElement('img');
+        Imagem.setAttribute('src', this.getAttribute('imagem'));
+        right.appendChild(Imagem);
+
+        componenteRoot.appendChild(left);
+        componenteRoot.appendChild(right);
+        
+        return componenteRoot;
+    }
+
+
+    style () {}
+}
+
+
+customElements.define('card-news', CardNews);
--- Desafios/Web Components/src/componentes/TituloDinamico.js
+++ Desafios/Web Components/src/componentes/TituloDinamico.js
@@ -2,9 +2,21 @@ class TituloDinamico extends HTMLElement {
   constructor() {
     super();
     const shadow = this.attachShadow({ mode: 'open' });
+
     // Criar a base do componente
+    const componenteRoot = document.createElement('h1');
+    componenteRoot.textContent = this.getAttribute('titulo');
+
     // Estilizar o componente
+    const style = document.createElement('style');
+    style.textContent = `
+      h1 {
+      color: red;
+      }`
+
     // Enviar para o shadow DOM
+    shadow.appendChild(style);
+    shadow.appendChild(componenteRoot);
     }
 }
 customElements.define('titulo-dinamico', TituloDinamico);
