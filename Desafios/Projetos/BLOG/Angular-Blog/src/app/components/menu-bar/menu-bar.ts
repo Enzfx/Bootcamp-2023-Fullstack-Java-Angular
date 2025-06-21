cda43bd63c1473899a38471e241c5cf3720eae1b
Enzo Freitas <genzofreitas@gmail.com>
2025-06-20 22:05:33 -0300

    Update

--- Desafios/Projetos/BLOG/Angular-Blog/src/app/components/menu-bar/menu-bar.ts
+++ Desafios/Projetos/BLOG/Angular-Blog/src/app/components/menu-bar/menu-bar.ts
@@ -2,10 +2,13 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-menu-bar',
-  imports: [],
   templateUrl: './menu-bar.html',
-  styleUrl: './menu-bar.css'
+  styleUrls: ['./menu-bar.css']
 })
-export class MenuBar {
+export class MenuBarComponent {
+  sidebarOpen = false;
 
+  toggleSidebar() {
+    this.sidebarOpen = !this.sidebarOpen;
+  }
 }
--- Desafios/Projetos/BLOG/Angular-Blog/src/app/pages/home/home.ts
+++ Desafios/Projetos/BLOG/Angular-Blog/src/app/pages/home/home.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 import { MenuTitle } from "../../components/menu-title/menu-title";
 import { BigCard } from "../../components/big-card/big-card";
 import { SmallCard } from "../../components/small-card/small-card";
+import { MenuBarComponent } from "../../components/menu-bar/menu-bar";
 
 @Component({
   selector: 'app-home',
-  imports: [CommonModule, MenuTitle, BigCard, SmallCard],
+  imports: [CommonModule, MenuTitle, BigCard, SmallCard, MenuBarComponent],
   templateUrl: './home.html',
   styleUrl: './home.css'
 })
