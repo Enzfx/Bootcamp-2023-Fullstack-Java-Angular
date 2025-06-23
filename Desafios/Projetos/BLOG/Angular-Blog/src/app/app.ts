84be3cbb75e6c3765c1fe28de9c089ca01f0f3c9
Enzo Freitas <genzofreitas@gmail.com>
2025-06-23 15:59:22 -0300

    Update

--- Desafios/Projetos/BLOG/Angular-Blog/src/app/app.ts
+++ Desafios/Projetos/BLOG/Angular-Blog/src/app/app.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { MenuBarComponent } from "./components/menu-bar/menu-bar";
 
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, CommonModule],
+  imports: [RouterOutlet, CommonModule, MenuBarComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
