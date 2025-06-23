60a457c753350285954d0f6abfd22cc5e441f8ac
Enzo Freitas <genzofreitas@gmail.com>
2025-06-23 18:40:32 -0300

    Testando a dinamica da pagina

--- Desafios/Projetos/BLOG/Angular-Blog/src/app/data/dataFake.ts
+++ Desafios/Projetos/BLOG/Angular-Blog/src/app/data/dataFake.ts
@@ -1,8 +1,14 @@
 export const dataFake = [
   {
-    "id": 1,
-    "photo":"",
-    "title": "", 
-    "description": ""
+    "id": "1",
+    "title": "Motorcycle", 
+    "description": "jsgojopjgpojojgjohjgohoghiohrho4",
+    "photoCover": "https://i.imgur.com/9e61AmN.png"
+  },
+  {
+    "id": "2",
+    "title": "Car", 
+    "description": "jsgojopjgpojojgjohjgohoghiohrho4",
+    "photoCover": "https://imgur.com/cd6940eb-c9c9-43c2-b1db-359246b615a4"
   }
 ]
--- Desafios/Projetos/BLOG/Angular-Blog/src/app/pages/content/content.ts
+++ Desafios/Projetos/BLOG/Angular-Blog/src/app/pages/content/content.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { dataFake } from '../../data/dataFake';
 
 @Component({
   selector: 'app-content',
@@ -7,5 +9,27 @@ import { Component } from '@angular/core';
   styleUrl: './content.css'
 })
 export class Content {
+  PhotoCover: string = '';
+  contentTitle: string = '';
+  contentDescription: string = '';
+  private id: string | null = '0';
 
+constructor(private route: ActivatedRoute){}
+ngOnInit(){
+  this.route.paramMap.subscribe(value =>
+      this.id = value.get('id')
+   )
+  this.setValuesToComponent(this.id);
+  }
+
+
+setValuesToComponent(id:string | null){
+    const result = dataFake.filter(article => article.id === id)[0]
+
+    this.contentTitle = result.title;
+    this.contentDescription = result.description;
+    this.PhotoCover = result.photoCover;
+
+  
+}
 }
