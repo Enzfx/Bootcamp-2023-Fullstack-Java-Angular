2d34822fe55b93e3ebea850c3cc693703e740558
Enzo Freitas <genzofreitas@gmail.com>
2025-06-27 10:57:31 -0300

    Updating Java folder files

---
+++ Java/java-anatomia-classes/src/edu/enzo/operadores/Atribuicao.java
@@ -0,0 +1,21 @@
+package edu.enzo.operadores;
+
+public class Atribuicao {
+    public static void main (String[] args){
+    //classe Operadores.java
+    String nome = "GLEYSON";
+    int idade = 22;
+    double peso = 68.5;
+    char sexo = 'M';
+    boolean doadorOrgao = false;
+    //Date dataNascimento = new Date(); // Data de nascimento (15 de agosto de 2001)
+    
+    // Exibindo os valores
+    System.out.println("Nome: " + nome);
+    System.out.println("Idade: " + idade);
+    System.out.println("Peso: " + peso);
+    System.out.println("Sexo: " + sexo);
+    System.out.println("Doador de Órgão: " + doadorOrgao);
+    }
+    
+}
---
+++ Java/java-anatomia-classes/src/edu/enzo/operadores/Unarios.java
@@ -0,0 +1,30 @@
+package edu.enzo.operadores;
+
+public class Unarios {
+    public static void main (String[] args){
+    
+    int numero = 5;
+    numero = - numero;        
+    System.out.println(numero);
+    // para tornar positivo o numero, basta multiplicar por -1
+    numero = numero *-1;
+    System.out.println(numero);
+
+    // numero = + numero;
+    // numero ++;
+    // System.out.println(numero);
+
+
+    // System.out.println(numero ++);// ops algo de errado não está certo
+
+    // System.out.println(numero);// agora sim, numero virou 7
+
+
+    // System.out.println(++ numero);
+
+    // boolean verdadeiro = true;
+
+    // System.out.println("Inverteu " + !verdadeiro);
+    // }
+    }
+}
