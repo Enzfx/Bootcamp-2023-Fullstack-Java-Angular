0c8a2d039c400ed980af77af97d39e1afbeecccc
Enzo Freitas <genzofreitas@gmail.com>
2025-07-05 19:49:30 -0300

    Alteraçao no Operadores/Aritmeticos

--- Java/Sintaxe/src/edu/enzo/operadores/Aritmeticos.java
+++ Java/Sintaxe/src/edu/enzo/operadores/Aritmeticos.java
@@ -3,15 +3,15 @@ package edu.enzo.operadores;
 public class Aritmeticos {
     public static void main(String[] args){
     //classe Operadores.java
-    double soma = 10.5 + 15.7;
-    int subtração = 113 - 25;
-    int multiplicacao = 20 * 7;
-    int divisao = 15 / 3;
-    int modulo = 18 % 3;
-    double resultado = (10 * 7) + (20/4);
+    // double soma = 10.5 + 15.7;
+    // int subtração = 113 - 25;
+    // int multiplicacao = 20 * 7;
+    // int divisao = 15 / 3;
+    // int modulo = 18 % 3;
+    // double resultado = (10 * 7) + (20/4);
 
-    //classe Operadores.java
-    String nomeCompleto = "LINGUAGEM" + "JAVA";
+    // //classe Operadores.java
+    // String nomeCompleto = "LINGUAGEM" + "JAVA";
     // Resultado do nomeCompleto: "LINGUAGEMJAVA"
             
     //qual o resultado das expressoes abaixo?
